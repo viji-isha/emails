abef65b88b87ef77607fb83ad731f3b540d7b642
Gaurav Bansal <gaurav3@gmail.com>
2016-09-25 23:15:55 +0800

    Hatha Yoga Confirmation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,7 +12,7 @@ import siphon   from 'siphon-media-query';
 const $ = plugins();
 const center = 'hk';
 const year = '2016';
-const wip = '201609-schedule';
+const wip = 'hatha-yoga-confirmation';
 const wip_path = center + '/' + year + '/' + wip + '.html';
 const base = 'src/pages/';
 const src = base + wip_path;
