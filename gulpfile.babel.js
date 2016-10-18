1a2fa744616f282d82ea22c125744666a4e036b8
YuvarajIshanga <g.yuvarajmail@gmail.com>
2016-10-18 23:20:06 +0800

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,9 +10,9 @@ import fs       from 'fs';
 import siphon   from 'siphon-media-query';
 
 const $ = plugins();
-const center = 'au';
+const center = 'hk';
 const year = '2016';
-const wip = 'sathsang-201610';
+const wip = 'Angamardhana-201618';
 const wip_path = center + '/' + year + '/' + wip + '.html';
 const base = 'src/pages/';
 const src = base + wip_path;
